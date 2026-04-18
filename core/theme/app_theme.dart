ca116863c0b9cd411b1c581e8ebf94efb3b616c1
Jiya0809 <jiyapatwari@gmail.com>
2026-04-19 02:01:20 +0530

    Create AppTheme class with light theme configuration

---
+++ core/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData light = ThemeData(
+    primaryColor: Colors.green,
+    scaffoldBackgroundColor: Colors.grey[100],
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.green,
+      foregroundColor: Colors.white,
+    ),
+  );
+}
